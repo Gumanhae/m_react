1f7a48d0aed2671b060bd43c09a9bde80605df0f
rnfmadkr@naver.com <rnfmadkr@naver.com>
2023-10-13 10:04:43 +0900

    movie list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from "react";
-import {useState, useEffect} from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail";
 
 import { hot } from 'react-hot-loader/root';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [])
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : 
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-            ))}
-        </select>
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+
+function Movie({coverImg, title, summary, genres}) {
+  return (
+    <div>
+      <img src={coverImg} />
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+
+  )
+}
+
+Movie.propTypes = {
+  coverImg : PropTypes.string.isRequired,
+  title : PropTypes.string.isrequired,
+  summary : PropTypes.string.isrequired,
+  genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
