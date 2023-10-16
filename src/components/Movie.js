4f235991590240010b130ae90bcd44eaac41adf8
rnfmadkr@naver.com <rnfmadkr@naver.com>
2023-10-16 13:57:17 +0900

    movie list + detail link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 
-function Movie({coverImg, title, summary, genres}) {
+function Movie({id, coverImg, title, summary, genres}) {
   return (
     <div>
       <img src={coverImg} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -22,6 +22,7 @@ function Movie({coverImg, title, summary, genres}) {
 }
 
 Movie.propTypes = {
+  id:PropTypes.number.isrequired,
   coverImg : PropTypes.string.isRequired,
   title : PropTypes.string.isrequired,
   summary : PropTypes.string.isrequired,
